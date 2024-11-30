9b5f93e6f8d9d96c768421a65d9a7e0fad354efb
TabGame <51984892+TabGame@users.noreply.github.com>
2024-11-30 13:15:43 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["yk.2o8.cc"];
+pub const PUBLIC_RS_PUB_KEY: &str = "xpMHlImf+nIVTGuQv03FoeJYURTtV6nSGmziVWMS7FR8nfGdS6gBbNTcbi94Ojszhe2rys3zSFCfn4PH4ZUnwg==";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
